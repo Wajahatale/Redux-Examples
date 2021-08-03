5fee80dfeb20473ac7c0668089dc310ef7ceeb6f
Wajahatale <42202926+Wajahatale@users.noreply.github.com>
2021-08-03 09:39:10 +0500

    Update App.js

--- wajahatApp/App.js
+++ wajahatApp/App.js
@@ -8,6 +8,9 @@
 
 import React, {Fragment, Component} from 'react';
 import {SafeAreaView, StyleSheet, View, Button, Text} from 'react-native';
+import { connect } from 'react-redux';
+import { changeCount } from './actions/counts';
+import { bindActionCreators } from 'redux';
 
 import {
   Colors,
@@ -17,23 +20,19 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 class App extends Component {
-  state = {count: 0};
+  // state = {count: 0};
   decrementCount() {
-    let {count} = this.state;
+    let {count, actions } = this.props;
     count--;
-    this.setState({
-      count,
-    });
+    actions.changeCount(count);
   }
   incrementCount() {
-    let {count} = this.state;
+    let {count, actions } = this.state;
     count++;
-    this.setState({
-      count,
-    });
+    actions.changeCount(count);
   }
   render() {
-    const {count} = this.state;
+    const {count} = this.props;
     return (
       <SafeAreaView>
         <View styles={styles.container}>
@@ -54,4 +53,17 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+const mapStateToProps = state => ({
+  count: state.count,
+});
+
+const ActionCreators = Object.assign(
+  {},
+  changeCount,
+);
+const mapDispatchToProps =dispatch => ({
+  actions: bindActionCreators(ActionCreators, dispatch),
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
